2b2fa6833e9eb925deb99702067e7be2692aab8d
YuliyaDzmitrovich <yuliya_dzmitrovich@epam.com>
2021-06-20 11:46:18 +0300

    Add runner for sub-tasks

---
+++ src/main/java/tasks/day3/Day3Runner.java
@@ -0,0 +1,19 @@
+package main.java.tasks.day3;
+
+public class Day3Runner {
+    public static void main(String[] args) {
+
+        //System.out.println("Задача: найти сумму элементов целочисленного массива, которые кратны числу текущего месяца");
+        //ReturnArray returnArray = new ReturnArray();
+        //returnArray.arrayMonth();
+        //System.out.println("Задача: найти сумму каждого n-ого элемента в целочисленном массиве");
+        //new SumOfElements().sum(4);
+        System.out.println("Задача: вывести новый целочисленный массив в обратном порядке");
+        new ReturnIntArray().intArray();
+        //System.out.println("Задача: вывести в консоль рисунок из символов");
+        //Picture picture = new Picture();
+        //picture.createPicture();
+
+
+    }
+}
