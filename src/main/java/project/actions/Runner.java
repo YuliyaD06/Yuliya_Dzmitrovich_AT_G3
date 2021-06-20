3c6a20ef49eeb0f3d3768be799b4fc1b90edbad2
YuliyaDzmitrovich <yuliya_dzmitrovich@epam.com>
2021-06-20 11:38:45 +0300

    Add Runner class

---
+++ src/main/java/project/actions/Runner.java
@@ -0,0 +1,14 @@
+package main.java.project.actions;
+
+import main.java.project.boxing.Bottle;
+
+public class Runner {
+    public static void main(String[] args){
+
+        Bottle theBottle1 = new Bottle(0.5);
+        theBottle1.open();
+        //factory.theBottle2.open();
+        //factory.theBottle3.open();
+
+    }
+}
