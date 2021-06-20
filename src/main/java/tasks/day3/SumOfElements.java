1e0ac03cf64db2924ece6d21bd6166f8800d1fb4
YuliyaDzmitrovich <yuliya_dzmitrovich@epam.com>
2021-06-20 14:05:56 +0300

    Changes to SumOfElemets class (sub-task is finished)

--- src/main/java/tasks/day3/SumOfElements.java
+++ src/main/java/tasks/day3/SumOfElements.java
@@ -1,12 +1,10 @@
 package main.java.tasks.day3;
-// -- написать метод, который подсчитает сумму каждого
-// n-ого элемента в целочисленном массиве, где n - целое число,
-// передаваемое в сигнатуру этого метода
+/*написать метод, который подсчитает сумму каждого
+n-ого элемента в целочисленном массиве, где n - целое число, передаваемое в сигнатуру этого метода*/
 
 import java.util.Random;
 
 public class SumOfElements {
-
     public void sum(int n) {
         System.out.println("n = " + n);
         int[] array = new int[10];
@@ -16,12 +14,24 @@ public class SumOfElements {
             System.out.println(array[i] = random.nextInt(20));
         }
         int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (i == n) {
-                sum = array[i];
-                System.out.println("Сумма каждого n-го элемента в массиве = " + sum);
-            }
+        int[] tempo = new int[array.length];
+        int counter = 0;
+
+        for (int i = n - 1; i < array.length; i += n) {
+            tempo[counter++] = array[i];
         }
+
+        int[] last = new int[counter];
+        System.out.println("Выбранный массив: ");
+        for (int i = 0; i < counter; i++) {
+            last[i] = tempo[i];
+            System.out.println(last[i]);
+        }
+
+        for (int i = 0; i < last.length; i++) {
+            sum += last[i];
+        }
+        System.out.println("Сумма каждого n-го элемента в массиве = " + sum);
     }
 
 }
