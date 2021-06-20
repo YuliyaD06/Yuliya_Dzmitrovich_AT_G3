2a73620f402c4bcf9e68560b23b580149542be88
YuliyaDzmitrovich <yuliya_dzmitrovich@epam.com>
2021-06-20 11:35:09 +0300

    Add SparklingWater class

---
+++ src/main/java/project/composition/SparklingWater.java
@@ -0,0 +1,51 @@
+package main.java.project.composition;
+//---- SparklingWater:
+//++наследуется от Water
+//++содеждит приватные поля boolean isOpened, Bubble[] bubbles
+//++содержит конструктор SparklingWater(), который вызывает внутренний метод isOpened();
+//++есть публичный метод void pump(Bubble[] bubbles), который сетает массив из пузырьков в воду
+//++есть публичный метод void setOpened(boolean isOpened), который меняет состояние воды на "открытое"
+//------есть приватный метод void isOpened(), который в новом потоке проверят состояние воды на "открытость" и в случае,
+// если она открыта запускает метод degas()
+//------ есть приватный метод degas(), который каждую секунду выпускает по партии пузырьков из рассчета 10 + 5 * температура_воды
+//------ есть публичный метод boolean isSparkle(), возвращающий true если в воде еще есть пузырьки газа
+//-- каждый метод должен содержать сообщение о том, что он делает, например:
+//System.out.printf("Warming water to: %s", temperature).println();
+//-- сообщения должны быть написаны 1-й строкой System.out.print* (пример см строкой выше)
+
+public class SparklingWater extends Water {
+    private boolean isOpened;
+    private Bubble[] bubbles;
+    public boolean isSparkle;
+
+    public SparklingWater(){
+        isOpened();
+    }
+    public void pump(Bubble[] bubble) {
+        bubbles = bubble;
+    }
+
+    public void degas() { //private or public
+        System.out.println("Starting degas..");
+        for (int i = 0; i < bubbles.length - 1; i++) {
+            this.bubbles[i].cramp();
+            this.bubbles[i] = null;
+        }
+        System.out.println("Degassing is finished.");
+    }
+    public void setOpened(boolean isOpened){
+        this.isOpened = true;
+    }
+
+    private void isOpened() {
+
+    }
+    public boolean isSparkle() {
+        return isSparkle;
+    }
+
+    @Override
+    public void cramp(Bubble[] bubbles) {
+
+    }
+}
