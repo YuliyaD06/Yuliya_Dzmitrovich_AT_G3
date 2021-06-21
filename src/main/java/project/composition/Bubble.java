d5473ab67696efd15ac6f5417406334181b0ee48
YuliyaDzmitrovich <yuliya_dzmitrovich@epam.com>
2021-06-21 22:55:45 +0300

    Updates to Bubble class

--- src/main/java/project/composition/Bubble.java
+++ src/main/java/project/composition/Bubble.java
@@ -1,32 +1,32 @@
 package main.java.project.composition;
-////++ Bubble:
-////++ содеждит приватные поля double volume = 03, String gas;
-////++ содержит конструктор Bubble(String gas), в который передается строка с названием газа пузырька
-////++ есть публичный метод void cramp(), который в консоль выводит слово "Cramp!"
-////++ есть публичный метод double getVolume(), возвращающий обьем пузырька
+/*Bubble:
+++ содеждит приватные поля double volume = 03, String gas;
+++ содержит конструктор Bubble(String gas), в который передается строка с названием газа пузырька
+++ есть публичный метод void cramp(), который в консоль выводит слово "Cramp!"
+++ есть публичный метод double getVolume(), возвращающий обьем пузырька*/
 
 public class Bubble {
     private double volume;
-    private String composition;
+    private String gas;
 
-    public Bubble(String composition){
+    public Bubble(String gas){
         this.volume = 0.3;
-        this.composition = composition;
+        this.gas = gas;
     }
 
     public static void cramp(){
         System.out.println("Cramp!");
-    }
-    public void setComposition(String composition) {
-        this.composition = composition;
-    }
-
-    public String getComposition() {
-        return composition;
     }
 
     public double getVolume() {
         return volume;
     }
 
+    public String getGas() {
+        return gas;
+    }
+
+    public void setGas(String gas) {
+        this.gas = gas;
+    }
 }
