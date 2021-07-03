bae74e4eb0749b27be9a149820d029cadf6a30b4
YuliyaDzmitrovich <yuliya_dzmitrovich@epam.com>
2021-07-03 17:06:36 +0300

    Update type package

--- src/main/java/project/type/Metal.java
+++ src/main/java/project/type/Metal.java
@@ -4,6 +4,9 @@ public class Metal extends Material {
 
     public Metal(double thermalConductivity, String color, double density){
         super (thermalConductivity, color, density);
+        this.setColor("Red");
+        this.setDensity(7.85);
+        this.setThermalConductivity(74.4);
         this.getThermalConductivity();
         this.getColor();
         this.getDensity();
--- src/main/java/project/type/Plastic.java
+++ src/main/java/project/type/Plastic.java
@@ -2,11 +2,16 @@ package main.java.project.type;
 
 public class Plastic extends Material {
 
+
     public Plastic(double thermalConductivity, String color, double density){
+
         super (thermalConductivity, color, density);
-        this.getThermalConductivity();
+        this.setThermalConductivity(0.1);
+        this.setColor("Purple");
+        this.setDensity(0.85);
         this.getColor();
         this.getDensity();
+        this.getThermalConductivity();
     }
 
 }
