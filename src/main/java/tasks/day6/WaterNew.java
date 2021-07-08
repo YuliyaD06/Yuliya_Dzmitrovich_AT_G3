2f38f3e0a9f0f63561e5cfe2ed78977db5b74b6a
YuliyaDzmitrovich <yuliya_dzmitrovich@epam.com>
2021-07-09 00:35:45 +0300

    Update sub-tasks

---
+++ src/main/java/tasks/day6/WaterNew.java
@@ -0,0 +1,28 @@
+package main.java.tasks.day6;
+
+
+public  class WaterNew {
+
+    private String color = "no";
+    private String smell = "no";
+
+
+    public WaterNew(String color, String smell) {
+        this.color = color;
+        this.smell = smell;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String isSmell() {
+        return smell;
+    }
+    public void setSmell(String smell) {
+        this.smell = smell;
+    }
+
+}
